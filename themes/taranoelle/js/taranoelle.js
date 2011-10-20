0866d9ed173d927e7216bf60c6f669ba74dba408
tilur <tilur@mindless.com>
2011-10-20 09:32:58 -0400

    Initial commit of project

---
+++ themes/taranoelle/js/taranoelle.js
@@ -0,0 +1,68 @@
+jQuery(document).ready(function() {
+	jQuery('body').height( jQuery(window).height() );
+
+	if (jQuery('#thumbnails-wrapper')) {
+		jQuery('#thumbnails-wrapper').bind({
+			mouseenter: function() {
+				jQuery('#thumbnails-wrapper').animate({
+					opacity: 1.00
+				}, 230, 'swing');
+
+			},
+			mouseleave: function() {
+				jQuery('#thumbnails-wrapper').animate({
+					opacity: 0.20
+				}, 230, 'swing');
+			}
+		});
+	}
+});
+
+function tnpLoadBG(type, image, style) {
+  if (type == 'gallery') {
+    // Need to do an image pre-load here so user doesn't have a bad experience.
+		jQuery('#preloader').show();
+		objImage = new Image();
+		objImage.src = '/sites/www.taranoellephotography.com/files/'+image;
+		objImage.onload = function() {
+			jQuery('#preloader').hide();
+	    output = 'background-image:url(/sites/www.taranoellephotography.com/files/'+image+');';
+
+			if (style != '') {
+				output += ' background-position:'+style;
+			}
+
+	    jQuery('#page-background').attr('style', output);
+		}
+  }
+}
+
+function tnpGalleryBG() {
+}
+
+function tnpScrollThumbnails(direction) {
+	offset = jQuery('#thumbnails-inner').offset();
+	height = jQuery('#thumbnails-inner').height();
+
+//alert(offset.top + " : " + height + " : " + (offset.top+height));
+
+	if (direction == '+' && offset.top == 95) { return; }
+	if (direction == '-' && (height+offset.top < 450)) { return; }
+
+	jQuery('#thumbnails-inner').animate({
+		top: direction+"=200"
+	}, 230, 'swing');
+}
+
+function tnpFade(type, direction) {
+	if (direction == 'out') { value = 0.25; }
+	else if (direction == 'in') { value = 1.00; }
+
+	switch (type) {
+		case 'thumbnails':
+			jQuery('#thumbnails-wrapper').animate({
+				opacity: value
+			}, 230, 'swing');
+			break;
+	}
+}
---
+++ themes/taranoelle/templates/node--image-gallery.tpl.php
@@ -0,0 +1,179 @@
+<?php
+//echo '<pre>';
+//print_r($node->field_gallery_image);
+//echo '</pre>';
+
+// $Id: node.tpl.php,v 1.2 2010/12/01 00:18:15 webchick Exp $
+
+/**
+ * @file
+ * Bartik's theme implementation to display a node.
+ *
+ * Available variables:
+ * - $title: the (sanitized) title of the node.
+ * - $content: An array of node items. Use render($content) to print them all,
+ *   or print a subset such as render($content['field_example']). Use
+ *   hide($content['field_example']) to temporarily suppress the printing of a
+ *   given element.
+ * - $user_picture: The node author's picture from user-picture.tpl.php.
+ * - $date: Formatted creation date. Preprocess functions can reformat it by
+ *   calling format_date() with the desired parameters on the $created variable.
+ * - $name: Themed username of node author output from theme_username().
+ * - $node_url: Direct url of the current node.
+ * - $display_submitted: Whether submission information should be displayed.
+ * - $submitted: Submission information created from $name and $date during
+ *   template_preprocess_node().
+ * - $classes: String of classes that can be used to style contextually through
+ *   CSS. It can be manipulated through the variable $classes_array from
+ *   preprocess functions. The default values can be one or more of the
+ *   following:
+ *   - node: The current template type, i.e., "theming hook".
+ *   - node-[type]: The current node type. For example, if the node is a
+ *     "Blog entry" it would result in "node-blog". Note that the machine
+ *     name will often be in a short form of the human readable label.
+ *   - node-teaser: Nodes in teaser form.
+ *   - node-preview: Nodes in preview mode.
+ *   The following are controlled through the node publishing options.
+ *   - node-promoted: Nodes promoted to the front page.
+ *   - node-sticky: Nodes ordered above other non-sticky nodes in teaser
+ *     listings.
+ *   - node-unpublished: Unpublished nodes visible only to administrators.
+ * - $title_prefix (array): An array containing additional output populated by
+ *   modules, intended to be displayed in front of the main title tag that
+ *   appears in the template.
+ * - $title_suffix (array): An array containing additional output populated by
+ *   modules, intended to be displayed after the main title tag that appears in
+ *   the template.
+ *
+ * Other variables:
+ * - $node: Full node object. Contains data that may not be safe.
+ * - $type: Node type, i.e. story, page, blog, etc.
+ * - $comment_count: Number of comments attached to the node.
+ * - $uid: User ID of the node author.
+ * - $created: Time the node was published formatted in Unix timestamp.
+ * - $classes_array: Array of html class attribute values. It is flattened
+ *   into a string within the variable $classes.
+ * - $zebra: Outputs either "even" or "odd". Useful for zebra striping in
+ *   teaser listings.
+ * - $id: Position of the node. Increments each time it's output.
+ *
+ * Node status variables:
+ * - $view_mode: View mode, e.g. 'full', 'teaser'...
+ * - $teaser: Flag for the teaser state (shortcut for $view_mode == 'teaser').
+ * - $page: Flag for the full page state.
+ * - $promote: Flag for front page promotion state.
+ * - $sticky: Flags for sticky post setting.
+ * - $status: Flag for published status.
+ * - $comment: State of comment settings for the node.
+ * - $readmore: Flags true if the teaser content of the node cannot hold the
+ *   main body content.
+ * - $is_front: Flags true when presented in the front page.
+ * - $logged_in: Flags true when the current user is a logged-in member.
+ * - $is_admin: Flags true when the current user is an administrator.
+ *
+ * Field variables: for each field instance attached to the node a corresponding
+ * variable is defined, e.g. $node->body becomes $body. When needing to access
+ * a field's raw values, developers/themers are strongly encouraged to use these
+ * variables. Otherwise they will have to explicitly specify the desired field
+ * language, e.g. $node->body['en'], thus overriding any language negotiation
+ * rule that was previously applied.
+ *
+ * @see template_preprocess()
+ * @see template_preprocess_node()
+ * @see template_process()
+ */
+
+  $image_id = false;
+  $array_keys = array_keys($_GET);
+  if (sizeof($array_keys) > 1) {
+    $image_id = $array_keys[1];
+  }
+
+  $imageStylesIn = $node->body['und'][0]['value'];
+  $imageStylesIn = explode("\n", $imageStylesIn);
+  foreach ($imageStylesIn AS $i => $imageStyle) {
+    $imageStyle = str_replace('centre', 'center', $imageStyle);
+    $imageStyle = explode('|', $imageStyle);
+    $imageStyles[$imageStyle[0]] = trim($imageStyle[1]).'|'.trim($imageStyle[2]);
+  }
+
+?>
+<div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+	<div id="preloader"></div>
+  <div id="thumbnails-wrapper">
+		<div class="thumbnails-top">
+			<div class="arrow-up" onClick="tnpScrollThumbnails('-');"></div>
+		</div>
+
+		<div id="thumbnails-inner">
+    <?php
+		$first = true;
+    foreach ($node->field_gallery_image['und'] AS $i => $image) {
+      $filename = $image['filename'];
+      $filepath = '/sites/www.taranoellephotography.com/files/styles/thumbnail/public/'.$image['filename'];
+      $style = '';
+
+
+      if (isset($imageStyles[$filename])) {
+				$imageStyle = explode('|', $imageStyles[$filename]);
+				// Vertial alignment is always the first element.
+				$alignVertical = $imageStyle[0];
+				
+				if (sizeof($imageStyle) === 2) { $alignHorizontal = $imageStyle[1]; }
+				else {$alignHorizontal = 'center'; }
+
+				$style = trim($alignHorizontal.' '.$alignVertical);
+      }
+
+			if ($first || ($image_id-1) == $i) {
+				$class = ' class="first"';
+				
+				$firstFilename = $filename;
+				$firstStyle = $style;
+				$firstClass = $class;
+
+				$first = false;
+			}
+			else { $class = ''; }
+
+      echo '<img src="'.$filepath.'" onClick="tnpLoadBG(\'gallery\', \''.$filename.'\', \''.$style.'\');"'.$class.'>';
+    }
+    ?>
+		</div>
+
+		<div class="thumbnails-bottom">
+			<div class="arrow-down" onClick="tnpScrollThumbnails('+');"></div>
+		</div>
+  </div>
+
+  <div class="content clearfix"<?php print $content_attributes; ?>>
+    <?php
+      // We hide the comments and links now so that we can render them later.
+      hide($content['comments']);
+      hide($content['links']);
+      hide($content);
+      //print render($content);
+    ?>
+  </div>
+
+</div>
+
+<script type="text/javascript">
+	tnpLoadBG('gallery', '<?php print $firstFilename?>', '<?php print $firstStyle?>');
+	jQuery('#header-wrapper').bind({
+		mouseenter: function() {
+			jQuery('#header-wrapper').animate({
+				opacity: 1.00
+			}, 230, 'swing');
+
+		},
+		mouseleave: function() {
+			jQuery('#header-wrapper').animate({
+				opacity: 0.20
+			}, 230, 'swing');
+		}
+	});
+	jQuery('#header-wrapper').animate({ opacity: 0.20 }, 230, 'swing');
+	jQuery('#thumbnails-wrapper').animate({ opacity: 0.20 }, 230, 'swing');
+
+</script>
